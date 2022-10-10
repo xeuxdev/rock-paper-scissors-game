688d1945e2e1d61ef64fad2d3bb20d320ecbeb4c
headbwoi <tochukwujohn24@gmail.com>
2022-10-10 16:30:04 +0100

    done with gameplay part of the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import GameContextProvider from "./components/contexts/GameContext"
-import { Header, GameDefault, ModalOpen, Modal } from "./components/index"
+import {
+  Header,
+  GameDefault,
+  ModalOpen,
+  Modal,
+  GamePlay,
+} from "./components/index"
 
 function App() {
   return (
     <GameContextProvider>
       <div className="container p-7 md:p-9  lg:p-11 ">
         <Header />
-        <GameDefault />
+        {/* <GameDefault /> */}
+        <GamePlay />
         <ModalOpen />
         <Modal />
       </div>
