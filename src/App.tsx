f7f10c0a2fb9534627fb7fcd391dcce17e994ee5
headbwoi <tochukwujohn24@gmail.com>
2022-10-10 12:32:41 +0100

    moved rules button to components

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,6 @@ function App() {
     <div className="container p-7 md:p-9  lg:p-11 ">
       <Header />
       <GameDefault />
-      <button
-        className="absolute bottom-0 left-1/2 -translate-x-1/2 md:left-[90%] uppercase w-32 h-11 grid place-items-center  text-Header font-bold border-2 border-Header rounded-lg mb-10 hover:text-white duration-300 hover:border-white"
-        aria-label="show Rules"
-      >
-        Rules
-      </button>
     </div>
   )
 }
---
+++ src/components/Modal/ModalOpen.tsx
@@ -0,0 +1,12 @@
+const ModalOpen = () => {
+  return (
+    <button
+      className="absolute bottom-0 left-1/2 -translate-x-1/2 md:left-[90%] uppercase w-32 h-11 grid place-items-center  text-Header font-bold border-2 border-Header rounded-lg mb-10 hover:text-white duration-300 hover:border-white"
+      aria-label="show Rules"
+    >
+      Rules
+    </button>
+  )
+}
+
+export default ModalOpen
