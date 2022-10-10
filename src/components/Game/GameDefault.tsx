9a1433f0c929fc700e7939cca91493d479b3c2e4
headbwoi <tochukwujohn24@gmail.com>
2022-10-10 10:14:08 +0100

    set traingle bg

--- src/components/Game/GameDefault.tsx
+++ src/components/Game/GameDefault.tsx
@@ -1,5 +1,11 @@
 const GameDefault = () => {
-  return <div>GameDefault</div>
+  return (
+    <div className="w-full md:max-w-[31.25rem] h-[18.75rem] md:h-[28.125rem] border-rock_gradient_1 border mx-auto relative">
+      <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-[url('/assets/bg-triangle.svg')] w-2/4 h-2/4  bg-center bg-no-repeat bg-contain"></div>
+    </div>
+  )
 }
 
 export default GameDefault
+200
+140
