32e158d70f1fdbe603faea7c9fc6e398f2e3a6c0
headbwoi <tochukwujohn24@gmail.com>
2022-10-10 20:46:24 +0100

    setup playerChoice state

--- src/components/contexts/GameContext.tsx
+++ src/components/contexts/GameContext.tsx
@@ -5,6 +5,8 @@ type GameContextType = {
   isModalOpen: boolean //toggles modal
   setisModalOpen: React.Dispatch<React.SetStateAction<boolean>> //modal toggler
   handleModalToggle: () => void //function to handle toggle
+  playerChoice: string
+  setPlayerChoice: React.Dispatch<React.SetStateAction<string>>
 }
 //type for children
 type GameContextProviderType = {
@@ -17,13 +19,20 @@ export const GameContext = createContext<GameContextType | null>(null)
 //provoder for context wrapper that wrapes entire application
 const GameContextProvider = ({ children }: GameContextProviderType) => {
   const [isModalOpen, setisModalOpen] = useState(false) //state for toggling modal
+  const [playerChoice, setPlayerChoice] = useState("") //state for setting player choice
   const handleModalToggle = () => {
     //function for toggling state
     setisModalOpen((prev) => !prev) //toggles state here
   }
   return (
     <GameContext.Provider //context provider for the entire app it wrappes
-      value={{ isModalOpen, setisModalOpen, handleModalToggle }} //values in the context
+      value={{
+        isModalOpen,
+        setisModalOpen,
+        handleModalToggle,
+        playerChoice,
+        setPlayerChoice,
+      }} //values in the context
     >
       {children}
     </GameContext.Provider>
