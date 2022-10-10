7570c78f7b125c19e3d271a10b789772aaf478c0
headbwoi <tochukwujohn24@gmail.com>
2022-10-10 02:10:27 +0100

    added extra config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+    },
     colors: {
       dark_Text: "hsl(229, 25%, 31%)",
       score_Text: "hsl(229, 64%, 46%)",
       Header: "hsl(217, 16%, 45%)",
-      /**
-       *
-       */
+      bg_gradient_1: "hsl(214, 47%, 23%)",
+      bg_gradient_2: "hsl(237, 49%, 15%)",
+      scissors_gradient_1: "hsl(39, 89%, 49%)",
+      scissors_gradient_2: "hsl(40, 84%, 53%)",
+      paper_gradient_1: "hsl(230, 89%, 62%)",
+      paper_gradient_2: "hsl(230, 89%, 65%)",
+      rock_gradient_1: "hsl(349, 71%, 52%)",
+      rock_gradient_2: "hsl(349, 70%, 56%)",
+      cyan: "hsl(189, 59%, 53%)",
+      cyan2: "hsl(189, 58%, 57%)",
     },
     fontFamily: {
       barlow: [`"Barlow Semi Condensed", sans-serif`],
