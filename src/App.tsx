efcb1aac5e0146604d1b6ee4ccd6c1c5b82bc211
headbwoi <tochukwujohn24@gmail.com>
2022-10-10 12:33:50 +0100

    exported modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Header, GameDefault } from "./components/index"
+import { Header, GameDefault, ModalOpen } from "./components/index"
 
 function App() {
   return (
     <div className="container p-7 md:p-9  lg:p-11 ">
       <Header />
       <GameDefault />
+      <ModalOpen />
     </div>
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Header } from "./Header/Header"
 export { default as GameDefault } from "./Game/GameDefault"
+export { default as ModalOpen } from "./Modal/ModalOpen"
