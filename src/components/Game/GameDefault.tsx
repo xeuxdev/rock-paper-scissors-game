2a094b4ddcda4590af85f1ea6d596f34f8b5b0d0
headbwoi <tochukwujohn24@gmail.com>
2022-10-10 17:25:54 +0100

    added comment to GameDefault

--- src/components/Game/GameDefault.tsx
+++ src/components/Game/GameDefault.tsx
@@ -5,6 +5,7 @@ const GameDefault = () => {
     <div className="w-full md:max-w-[31.25rem] h-[18.75rem] md:h-[28.125rem] mx-auto relative">
       <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-[url('/assets/bg-triangle.svg')] w-2/4 h-2/4  bg-center bg-no-repeat bg-contain"></div>
       <div className="flex flex-wrap justify-between">
+        {/* paper */}
         <motion.button
           className="w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-paper_gradient_2 relative grid place-items-center border-b-[10px] border-b-paper_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-paper_gradient_1"
           aria-label="select for paper"
@@ -22,6 +23,7 @@ const GameDefault = () => {
           </div>
         </motion.button>
 
+        {/* scissors */}
         <motion.button
           className="w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-scissors_gradient_2 relative grid place-items-center border-b-[10px] border-b-scissors_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-scissors_gradient_1 self-end ml-auto"
           aria-label="select for scissors"
@@ -39,6 +41,7 @@ const GameDefault = () => {
           </div>
         </motion.button>
 
+        {/* rock */}
         <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
           <motion.button
             className="w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-rock_gradient_2 grid place-items-center border-b-[10px] border-b-rock_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-rock_gradient_1"
