d2182760e282f20a9c60f7ae4c20813647885787
headbwoi <tochukwujohn24@gmail.com>
2022-10-10 09:58:49 +0100

    moved game export to export file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Header } from "./components/index"
+import { Header, GameDefault } from "./components/index"
 
 function App() {
   return (
     <div className="container p-7 md:p-9 lg:p-11">
       <Header />
+      <GameDefault />
     </div>
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Header } from "./Header/Header"
+export { default as GameDefault } from "./Game/GameDefault"
