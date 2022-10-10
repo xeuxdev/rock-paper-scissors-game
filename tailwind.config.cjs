2e05ee3b5a73777eb3e7027dbe553cabf69b3366
headbwoi <tochukwujohn24@gmail.com>
2022-10-10 02:01:50 +0100

    added text colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      dark_Text: "hsl(229, 25%, 31%)",
+      score_Text: "hsl(229, 64%, 46%)",
+      Header: "hsl(217, 16%, 45%)",
+      /**
+       *
+       */
+    },
+    fontFamily: {
+      barlow: [`"Barlow Semi Condensed", sans-serif`],
+    },
     extend: {},
   },
   plugins: [],
