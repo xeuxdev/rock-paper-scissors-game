a2a8c587b53a4a396e5b32995a7a86b46c25e1c1
headbwoi <tochukwujohn24@gmail.com>
2022-10-10 04:06:52 +0100

    added hosttel

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 const Header = () => {
   return (
-    <div className=" md:max-w-[43.75rem] rounded-lg md:rounded-xl border-[3px] border-Header">
+    <div className="w-full md:max-w-[43.75rem] mx-auto rounded-lg md:rounded-xl border-[3px] border-Header h-24 md:h-36 p-4">
+      <div className="logo pl-4">
+        <img src="assets/logo.svg" alt="" />
+      </div>
       Header
     </div>
   )
