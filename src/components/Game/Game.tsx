24504a947018ee84329c96362cfd792a008a8bdb
headbwoi <tochukwujohn24@gmail.com>
2022-10-11 08:23:18 +0100

    moved Computer Ai to GamePlay

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -5,22 +5,10 @@ import { GameDefault, GamePlay, GameResults } from "../index"
 const Game = () => {
   const { playerChoice, setAiChoice, setScore } = useContext(GameContext)
 
-  const aiOptions: string[] = ["paper", "scissors", "rock"]
-
-  const randomize = () => {
-    let randomNumber = Math.ceil(Math.round(Math.random() * 3))
-    console.log(randomNumber)
-    return randomNumber
-  }
-  console.log(aiOptions)
-  console.log(randomize())
-
   return (
     <div>
       {playerChoice === "" ? <GameDefault /> : <GamePlay />}
-      <button onClick={() => randomize()}>click</button>
-      {/* <GameDefault /> */}
-      {/* <GamePlay /> */}
+
       {/* <GameResults /> */}
     </div>
   )
--- src/components/Game/GamePlay.tsx
+++ src/components/Game/GamePlay.tsx
@@ -1,8 +1,27 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { GameContext } from "../contexts/GameContext"
+import { motion } from "framer-motion"
 
 const GamePlay = () => {
-  const { aiChoice, playerChoice } = useContext(GameContext)
+  const { aiChoice, playerChoice, setAiChoice } = useContext(GameContext)
+  const aiOptions: string[] = ["paper", "scissors", "rock"]
+
+  const randomize = () => {
+    let randomNumber = Math.ceil(Math.round(Math.random() * 3))
+    return randomNumber
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      randomize() !== null
+        ? setAiChoice(aiOptions[randomize()])
+        : setAiChoice("")
+    }, 1500)
+    return () => {
+      timeout
+    }
+  }, [])
+
   return (
     <div className="w-full md:max-w-[31.25rem] h-[18.75rem] md:h-[28.125rem] mx-auto relative">
       <div className="flex flex-wrap justify-between">
@@ -27,12 +46,12 @@ const GamePlay = () => {
         <div className="flex flex-col md:flex-col-reverse items-center">
           {aiChoice === "" ? (
             <div
-              className="bg-bg_gradient_1 w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full relative grid place-items-center shadow-lg hover:cursor-pointer outline-none"
+              className="bg-bg_gradient_1 w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full relative grid place-items-center shadow-lg hover:cursor-pointer outline-none animate-pulse"
               aria-label="select for scissors"
             ></div>
           ) : (
-            <div
-              className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${aiChoice}_gradient_2 relative grid place-items-center border-b-[10px] border-b-${aiChoice}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${aiChoice}_gradient_1 self-end ml-auto`}
+            <motion.div
+              className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${aiChoice}_gradient_2 relative grid place-items-center border-b-[10px] border-b-${aiChoice}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${aiChoice}_gradient_1 self-end ml-auto `}
               aria-label={`the house picked ${aiChoice}`}
             >
               <div className="bg-white w-[6.25rem] h-[6.25rem] md:w-[9.6875rem] md:h-[9.6875rem] rounded-full grid place-items-center border-t-[10px] border-t-bg_gradient_1/30">
@@ -42,7 +61,7 @@ const GamePlay = () => {
                   className="md:w-16 md:h-20"
                 />
               </div>
-            </div>
+            </motion.div>
           )}
 
           <p className="text-white text-sm md:text-lg font-bold mt-8 md:mt-0 md:mb-10">
