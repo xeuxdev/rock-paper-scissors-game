d0bfb5bddd058229c33cca05f5b4e35ed30fe9bd
headbwoi <tochukwujohn24@gmail.com>
2022-10-11 07:10:22 +0100

    refactored GAmePlay component

--- src/components/Game/GamePlay.tsx
+++ src/components/Game/GamePlay.tsx
@@ -2,22 +2,19 @@ import { useContext } from "react"
 import { GameContext } from "../contexts/GameContext"
 
 const GamePlay = () => {
-  const selectedChoice = useContext(GameContext)
-  const player = selectedChoice?.playerChoice
-  const AI = selectedChoice?.aiChoice
   const { aiChoice, playerChoice, setAiChoice } = useContext(GameContext)
   return (
     <div className="w-full md:max-w-[31.25rem] h-[18.75rem] md:h-[28.125rem] mx-auto relative">
       <div className="flex flex-wrap justify-between">
         <div className="flex flex-col md:flex-col-reverse items-center">
           <div
-            className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${player}_gradient_2 relative grid place-items-center border-b-[10px] border-b-${player}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${player}_gradient_1`}
+            className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${playerChoice}_gradient_2 relative grid place-items-center border-b-[10px] border-b-${playerChoice}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${playerChoice}_gradient_1`}
             aria-label="YOU PICKED"
           >
             <div className="bg-white w-[6.25rem] h-[6.25rem] md:w-[9.6875rem] md:h-[9.6875rem] rounded-full grid place-items-center border-t-[10px] border-t-bg_gradient_1/30">
               <img
-                src={`/assets/icon-${player}.svg`}
-                alt={`${player} selected`}
+                src={`/assets/icon-${playerChoice}.svg`}
+                alt={`${playerChoice} selected`}
                 className="md:w-16 md:h-20"
               />
             </div>
@@ -28,7 +25,7 @@ const GamePlay = () => {
         </div>
 
         <div className="flex flex-col md:flex-col-reverse items-center">
-          {AI === "" ? (
+          {aiChoice === "" ? (
             <div
               className="bg-bg_gradient_1 w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full relative grid place-items-center shadow-lg hover:cursor-pointer outline-none"
               aria-label="select for scissors"
