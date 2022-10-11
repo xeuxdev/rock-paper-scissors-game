d2a0c075555f56353152ed5e6ceb76b2224bf9ca
headbwoi <tochukwujohn24@gmail.com>
2022-10-11 07:37:41 +0100

    added aichoice to gameplay

--- src/components/Game/GamePlay.tsx
+++ src/components/Game/GamePlay.tsx
@@ -32,13 +32,13 @@ const GamePlay = () => {
             ></div>
           ) : (
             <div
-              className="w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-scissors_gradient_2 relative grid place-items-center border-b-[10px] border-b-scissors_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-scissors_gradient_1 self-end ml-auto"
-              aria-label="the house picked"
+              className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${aiChoice}_gradient_2 relative grid place-items-center border-b-[10px] border-b-${aiChoice}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${aiChoice}_gradient_1 self-end ml-auto`}
+              aria-label={`the house picked ${aiChoice}`}
             >
               <div className="bg-white w-[6.25rem] h-[6.25rem] md:w-[9.6875rem] md:h-[9.6875rem] rounded-full grid place-items-center border-t-[10px] border-t-bg_gradient_1/30">
                 <img
-                  src="/assets/icon-scissors.svg"
-                  alt="select for scissors"
+                  src={`/assets/icon-${aiChoice}.svg`}
+                  alt={`the house picked ${aiChoice}`}
                   className="md:w-16 md:h-20"
                 />
               </div>
