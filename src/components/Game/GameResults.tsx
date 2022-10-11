01f4cad51b24a58c2299784895348b92ceff9b19
headbwoi <tochukwujohn24@gmail.com>
2022-10-11 23:13:45 +0100

    fixed animation

--- src/components/Game/GameResults.tsx
+++ src/components/Game/GameResults.tsx
@@ -35,8 +35,10 @@ const GameResults = () => {
       randomNumber !== undefined
         ? setAiChoice(aiOptions[randomNumber])
         : setAiChoice("")
-      checkLogic()
     }, 1500)
+    setTimeout(() => {
+      checkLogic()
+    }, 1600)
     return () => {
       timeout
     }
@@ -58,8 +60,7 @@ const GameResults = () => {
             className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${playerChoice}_gradient_2 relative grid place-items-center border-b-[10px] border-b-${playerChoice}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${playerChoice}_gradient_1`}
             aria-label="YOU PICKED"
           >
-            {gameResult === "YOU WIN" ? <GameRipple /> : ""}
-            {/* <GameRipple /> */}
+            {gameResult === "YOU WIN" && <GameRipple />}
             <div className="bg-white w-[6.25rem] h-[6.25rem] md:w-[9.6875rem] md:h-[9.6875rem] rounded-full grid place-items-center border-t-[10px] border-t-bg_gradient_1/30">
               <img
                 src={`/assets/icon-${playerChoice}.svg`}
@@ -98,7 +99,7 @@ const GameResults = () => {
               className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${aiChoice}_gradient_2 relative grid place-items-center border-b-[10px] border-b-${aiChoice}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${aiChoice}_gradient_1 self-end ml-auto `}
               aria-label={`the house picked ${aiChoice}`}
             >
-              {gameResult === "YOU LOSE" ? <GameRipple /> : ""}
+              {gameResult === "YOU LOSE" && <GameRipple />}
               <div className="bg-white w-[6.25rem] h-[6.25rem] md:w-[9.6875rem] md:h-[9.6875rem] rounded-full grid place-items-center border-t-[10px] border-t-bg_gradient_1/30">
                 <img
                   src={`/assets/icon-${aiChoice}.svg`}
