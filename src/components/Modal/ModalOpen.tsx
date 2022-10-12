35a8388efc1ed215758bd2aaa85a3099652730d7
headbwoi <tochukwujohn24@gmail.com>
2022-10-13 00:18:32 +0100

    edited rukes and reset buttons

--- src/components/Modal/ModalOpen.tsx
+++ src/components/Modal/ModalOpen.tsx
@@ -5,7 +5,7 @@ const ModalOpen = () => {
   const { handleModalToggle } = useContext(GameContext) //imports context values
   return (
     <button
-      className="absolute bottom-0 left-2/4 md:left-[80%] uppercase w-32 h-11 grid place-items-center  text-Header font-bold border-2 border-Header rounded-lg mb-10 hover:text-white duration-300 hover:border-white z-30 mt-6"
+      className="absolute bottom-0 left-[60%] md:left-[80%] uppercase w-32 h-11 grid place-items-center  text-Header font-bold border-2 border-Header rounded-lg mb-10 hover:text-white duration-300 hover:border-white z-30 mt-6"
       aria-label="show Rules"
       onClick={handleModalToggle} ////when clicked call the handleModalToggle function
     >
--- src/components/Reset/Reset.tsx
+++ src/components/Reset/Reset.tsx
@@ -10,7 +10,7 @@ const Reset = () => {
   }
   return (
     <button
-      className="absolute bottom-0 right-2/4  md:right-[80%] md:-translate-x-0 uppercase w-32 h-11 grid place-items-center  text-Header font-bold border-2 border-Header rounded-lg mb-10 hover:text-white duration-300 hover:border-white z-30 mt-6"
+      className="absolute bottom-0 right-[60%]  md:right-[80%] md:-translate-x-0 uppercase w-32 h-11 grid place-items-center  text-Header font-bold border-2 border-Header rounded-lg mb-10 hover:text-white duration-300 hover:border-white z-30 mt-6"
       aria-label="show Rules"
       onClick={() => handleResetScores} ////when clicked call the resets score
     >
