95ff55369ddefecd6d9141958a51f513def86aad
headbwoi <tochukwujohn24@gmail.com>
2022-10-12 07:57:49 +0100

    changed result animation edited played text

--- src/components/Game/GameResults.tsx
+++ src/components/Game/GameResults.tsx
@@ -60,11 +60,11 @@ const GameResults = () => {
     setGameResult("")
   }
   return (
-    <div className="w-full sm:max-w-[31.25rem] md:max-w-[45.25rem] h-[18.75rem] md:h-[28.125rem] mx-auto relative">
+    <div className="w-full sm:max-w-[31.25rem] md:max-w-[48.25rem] h-[18.75rem] md:h-[28.125rem] mx-auto relative">
       <div className="flex items-center justify-between">
         <div className="flex flex-col md:flex-col-reverse items-center">
           <div
-            className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${playerChoice}_gradient_2 relative grid place-items-center border-b-[10px] border-b-${playerChoice}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${playerChoice}_gradient_1`}
+            className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${playerChoice}_gradient_2  grid place-items-center border-b-[10px] border-b-${playerChoice}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${playerChoice}_gradient_1`}
             aria-label="YOU playerED"
           >
             {gameResult === "YOU WIN" && <GameRipple />}
@@ -76,7 +76,7 @@ const GameResults = () => {
               />
             </div>
           </div>
-          <p className="text-white text-sm md:text-lg font-bold mt-8 md:mt-0 md:mb-10">
+          <p className="text-white text-sm md:text-xl font-bold mt-8 md:mt-0 md:mb-10">
             YOU PLAYED
           </p>
         </div>
@@ -109,7 +109,7 @@ const GameResults = () => {
             ></div>
           ) : (
             <motion.div
-              className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${aiChoice}_gradient_2 relative grid place-items-center border-b-[10px] border-b-${aiChoice}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${aiChoice}_gradient_1 self-end ml-auto `}
+              className={`w-[8.75rem] h-[8.75rem] md:w-[12.5rem] md:h-[12.5rem] rounded-full bg-${aiChoice}_gradient_2  grid place-items-center border-b-[10px] border-b-${aiChoice}_gradient_1 shadow-lg hover:cursor-pointer outline-none focus-visible:ring-2 focus-visible:ring-${aiChoice}_gradient_1 self-end ml-auto `}
               aria-label={`the house playered ${aiChoice}`}
             >
               {gameResult === "YOU LOSE" && <GameRipple />}
@@ -123,7 +123,7 @@ const GameResults = () => {
             </motion.div>
           )}
 
-          <p className="text-white text-sm md:text-lg font-bold mt-8 md:mt-0 md:mb-10">
+          <p className="text-white text-sm md:text-xl font-bold mt-8 md:mt-0 md:mb-10">
             THE HOUSE PLAYED
           </p>
         </div>
