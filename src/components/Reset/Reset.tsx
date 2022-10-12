32bb734d1116136a810f736a88583155cf1c5327
headbwoi <tochukwujohn24@gmail.com>
2022-10-12 20:20:11 +0100

    updated reset

--- src/components/Reset/Reset.tsx
+++ src/components/Reset/Reset.tsx
@@ -2,12 +2,17 @@ import { useContext } from "react"
 import { GameContext } from "../contexts/GameContext"
 
 const Reset = () => {
-  const { setScore } = useContext(GameContext)
+  const { setPlayerScore, setAiScore } = useContext(GameContext)
+
+  const handleResetScores = () => {
+    setPlayerScore(0)
+    setAiScore(0)
+  }
   return (
     <button
       className="absolute bottom-0 right-2/4  md:right-[80%] md:-translate-x-0 uppercase w-32 h-11 grid place-items-center  text-Header font-bold border-2 border-Header rounded-lg mb-10 hover:text-white duration-300 hover:border-white z-30 mt-6"
       aria-label="show Rules"
-      onClick={() => setScore(0)} ////when clicked call the resets score
+      onClick={() => handleResetScores} ////when clicked call the resets score
     >
       Reset
     </button>
