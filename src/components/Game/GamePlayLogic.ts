22021663980e66bc8d604dfc275ff91a82b74b27
headbwoi <tochukwujohn24@gmail.com>
2022-10-14 03:03:57 +0100

    improved logic

--- src/components/Game/GamePlayLogic.ts
+++ src/components/Game/GamePlayLogic.ts
@@ -6,25 +6,34 @@ const randomOption = (arr: string[]) => Math.floor(Math.random() * arr.length)
 
 const whoWinsTheGame = (player: string, bot: string) => {
   const result =
-    player === "rock" && bot === "paper" //checks for player == rock
-      ? "YOU LOSE"
-      : player === "rock" && bot === "scissors"
-      ? "YOU WIN"
-      : player === "rock" && bot === "rock"
+    // player === "rock" && bot === "paper" //checks for player == rock
+    //   ? "YOU LOSE"
+    //   : player === "rock" && bot === "scissors"
+    //   ? "YOU WIN"
+    //   : player === "rock" && bot === "rock"
+    //   ? "It's A TIE"
+    //   : player === "paper" && bot === "paper" //checls for player == paper
+    //   ? "It's A TIE"
+    //   : player === "paper" && bot === "scissors"
+    //   ? "YOU LOSE"
+    //   : player === "paper" && bot === "rock"
+    //   ? "YOU WIN"
+    //   : player === "scissors" && bot === "paper" //checks for player == scissors
+    //   ? "YOU WIN"
+    //   : player === "scissors" && bot === "scissors"
+    //   ? "It's A TIE"
+    //   : player === "scissors" && bot === "rock"
+    //   ? "YOU LOSE"
+    //   : undefined
+    (player === "scissors" && bot === "scissors") ||
+    (player === "paper" && bot === "paper") ||
+    (player === "rock" && bot === "rock")
       ? "It's A TIE"
-      : player === "paper" && bot === "paper" //checls for player == paper
-      ? "It's A TIE"
-      : player === "paper" && bot === "scissors"
-      ? "YOU LOSE"
-      : player === "paper" && bot === "rock"
+      : (player === "rock" && bot === "scissors") ||
+        (player === "paper" && bot === "rock") ||
+        (player === "scissors" && bot === "paper")
       ? "YOU WIN"
-      : player === "scissors" && bot === "paper" //checks for player == scissors
-      ? "YOU WIN"
-      : player === "scissors" && bot === "scissors"
-      ? "It's A TIE"
-      : player === "scissors" && bot === "rock"
-      ? "YOU LOSE"
-      : undefined
+      : "YOU LOSE"
   return result
 }
 
