eb590d262806b3fa3092e77195355397ac527a9f
Headbwoi <headbwoi25@gmail.com>
2023-05-15 00:07:57 +0100

    pos fux

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,17 +5,17 @@ const app = express()
 import { Server } from "socket.io"
 import cors from "cors"
 
-const server = http.createServer(app)
-const io = new Server(server, {
-  cors: {
-    origin: ["https://rps-game-et-moi.netlify.app/"],
-    methods: ["GET", "Post"],
-  },
-})
-
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: ["https://rps-game-et-moi.netlify.app"],
+    methods: ["GET", "Post"],
+  },
+})
 
 app.get("/api/", (req, res) => {
   res.status(200).json({ message: "OK" })
