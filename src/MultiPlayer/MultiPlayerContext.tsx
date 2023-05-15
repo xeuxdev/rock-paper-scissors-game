149538acf83812abdd5b425830038ea7803f2fe4
Headbwoi <headbwoi25@gmail.com>
2023-05-15 05:28:42 +0100

    fix shit url

--- src/MultiPlayer/MultiPlayerContext.tsx
+++ src/MultiPlayer/MultiPlayerContext.tsx
@@ -32,8 +32,8 @@ export const MultiPlayerGameContext = createContext<MultiPlayerGameContextType>(
   {} as MultiPlayerGameContextType
 )
 
-export const socket = io("http://localhost:3000")
-// export const socket = io("https://rps-game-ekn0.onrender.com")
+// export const socket = io("http://localhost:3000")
+export const socket = io("https://rps-game-ekn0.onrender.com")
 
 //provoder for context wrapper that wrapes entire application
 const MultiPlayerGameContextProvider = ({
